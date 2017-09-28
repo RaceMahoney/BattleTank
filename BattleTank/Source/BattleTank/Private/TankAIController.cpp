6eab5fca5cb56177c81519bb9d8c61e28e66754c
Race Mahoney <race.mahoney@gmail.com>
2017-09-27 22:40:27 -0400

    Fixed bug where TankAI did not move

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,10 +1,8 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "TankAIController.h"
 #include "TankAimingComponent.h"
 #include "Engine/World.h"
-
 
 void ATankAIController::BeginPlay() {
 	Super::BeginPlay();
@@ -17,7 +15,10 @@ void ATankAIController::Tick(float DeltaTime) {
 	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
 	auto ControlledTank = GetPawn();
 
-	if (!ensure(PlayerTank && ControlledTank)) {
+
+
+	if (!ensure(PlayerTank && ControlledTank)) { return; }
+
 		//Move Towards Player
 		MoveToActor(PlayerTank, AcceptenceRadius); //TODO check radius in cm
 
@@ -27,7 +28,6 @@ void ATankAIController::Tick(float DeltaTime) {
 		AimingComponent->AimAt(PlayerTank->GetActorLocation());
 
 		AimingComponent->Fire(); 
-	}
 	
 }
 
